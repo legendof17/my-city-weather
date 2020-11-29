6ff2cacb8fe05980f3df7cb654435645033f5b25
Jaffer Ali <jafferali150@gmail.com>
2020-11-30 01:25:41 +0530

    added getweather api

---
+++ src/api/getweather.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+export async function Getweather(woeid) {
+    const response = await axios('https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/' + woeid + '/');
+    console.log(response.data.consolidated_weather[0].weather_state_name);
+    console.log(response.data.consolidated_weather[0].weather_state_abbr);
+    console.log(response.data.consolidated_weather[0].humidity);
+    console.log(response.data.consolidated_weather[0].predictability);
+    console.log(response.data.consolidated_weather[0].min_temp);
+    console.log(response.data.consolidated_weather[0].max_temp);
+}
--- src/api/getwoeid.js
+++ src/api/getwoeid.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
+import { Getweather } from './getweather'
 
 export async function Getwoeid(city) {
-    const res = await axios('https://cors-anywhere.herokuapp.com/' + 'https://www.metaweather.com/api/location/search/?query=' + city);
-    console.log(res.data[0].woeid);
+    const res = await axios('https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/search/?query=' + city);
+    Getweather(res.data[0].woeid);
+    // console.log(res.data[0].woeid);
 }
