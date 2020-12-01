795ac23c0f8b8b1ed3722dfc8f596ea5766e7446
Jaffer Ali <jafferali150@gmail.com>
2020-12-02 00:53:15 +0530

    Rwrkd Route mtds, rm bsname, uptd /docs pack.json

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
-import { HashRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import Home from './pages';
 import Weather from './pages/weather';
 
 
 function App() {
   return (
-    <Router basename={process.env.PUBLIC_URL}>
+    <Router>
       <Navbar />
       <Switch>
         <Route path='/' exact component={Home} />
         <Route path='/weather' exact component={Weather} />
-        <Redirect to='/' />
+        <Redirect path='*' to='/' />
       </Switch>
     </Router>
   );
